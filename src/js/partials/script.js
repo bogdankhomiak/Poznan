c165ab7a8a0633b1ad02e500c9564e1bada33c80
bogdan <peculiarsev@gmail.com>
2017-09-04 12:13:57 +0300

    menu-fixed

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -1,4 +1,8 @@
 "use strict";
 $(document).ready(function () {
-
+    var heightMenu = $(".menu").outerHeight();
+    var menuItem = $(".menu__link");
+    menuItem.click(function () {
+        $(this).attr("href").scrollTop();
+    });
 });

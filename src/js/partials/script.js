943e19500bd4958985638cc444bb363d3b6d903c
bogdan <peculiarsev@gmail.com>
2017-09-07 15:44:56 +0300

    init

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -1,8 +1,4 @@
 "use strict";
 $(document).ready(function () {
-    var heightMenu = $(".menu").outerHeight();
-    var menuItem = $(".menu__link");
-    menuItem.click(function () {
-        $(this).attr("href").scrollTop();
-    });
+
 });
